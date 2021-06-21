7eed2d31bed50e876f5e23f48c183e94fba27e96
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2021-06-21 15:04:52 +0300

    Commit first

--- src/main/java/by/training/xml/entity/InternetTariff.java
+++ src/main/java/by/training/xml/entity/InternetTariff.java
@@ -1,4 +1,48 @@
 package by.training.xml.entity;
 
-public class InternetTariff {
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class InternetTariff extends Tariff{
+
+    private int internetTraffic;
+
+    public InternetTariff(int internetTraffic) {
+        this.internetTraffic = internetTraffic;
+    }
+
+    public InternetTariff(int payroll, int connectionPay, LocalDate introductionTime, int internetTraffic) {
+        super(payroll, connectionPay, introductionTime);
+        this.internetTraffic = internetTraffic;
+    }
+
+    public int getInternetTraffic() {
+        return internetTraffic;
+    }
+
+    public void setInternetTraffic(int internetTraffic) {
+        this.internetTraffic = internetTraffic;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        InternetTariff that = (InternetTariff) o;
+        return internetTraffic == that.internetTraffic;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), internetTraffic);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("InternetTariff{");
+        sb.append("internetTraffic=").append(internetTraffic);
+        sb.append('}');
+        return sb.toString();
+    }
 }
